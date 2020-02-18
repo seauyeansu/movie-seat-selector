143079dc07bf9e4fdda9fdacc1ceebaf19c2f3df
giselle <seauyeansu@gmail.com>
2020-02-18 10:45:41 +0100

    latest version before deleting package-lock.json and node_modules

--- app/store/reducer.js
+++ app/store/reducer.js
@@ -4,9 +4,24 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
+      return {
+          counter: state.counter + 1
+      }
+    }
+    if (action.type === 'DECREMENT') {
+      return {
+          counter: state.counter - 1
+      }
+    }
+    if (action.type === 'ADD5') {
+      return {
+        counter: state.counter + 5
+      }
+    }
+    if (action.type === 'SUBTRACT5') {
+      return {
+        counter: state.counter - 5
+      }
     }
     return state;
 };

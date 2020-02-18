95222d791c1e95736b200f4662a9e381d7534df5
giselle <seauyeansu@gmail.com>
2020-02-18 11:51:18 +0100

    added dropdown

--- app/components/CounterOutput/CounterOutput.js
+++ app/components/CounterOutput/CounterOutput.js
@@ -3,9 +3,24 @@ import React from 'react';
 import './CounterOutput.css';
 
 const counterOutput = (props) => (
-    <div className="CounterOutput">
-        Current Counter: {props.value}
+  <div className="CounterOutput and FlightSelection">
+    <div className="FlightSelection">
+      <form>
+        <label for="flights">Choose Your Flight:</label>
+        <select id="flights" name="flights">
+          <option value="morning">02.08. - 09:00</option>
+          <option value="afternoon">02.08. - 13:00</option>
+          <option value="evening">02.08. - 17:00</option>
+          <option value="night">02.08. - 21:00</option>
+        </select>
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
+
+  <div className="CounterOutput">
+    You have selected {props.value} seats for a price of EUR {props.value}.
+  </div>
+  </div>
 );
 
 export default counterOutput;

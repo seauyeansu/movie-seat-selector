4514cfa3192145ef7a56a06a15f9b3593f6e4e6e
giselle <seauyeansu@gmail.com>
2020-02-25 13:46:32 +0100

    added seat labels

--- src/components/SeatControl/SeatControl.js
+++ src/components/SeatControl/SeatControl.js
@@ -8,7 +8,22 @@ const seatControl = (props) => (
         <h1>Please select a seat</h1>
     </div>
 
+  <div class="fuselage">
   <div class="row">
+    <div class="label">A</div>
+    <div class="label">B</div>
+    <div class="label">C</div>
+    <div class="label">D</div>
+    <div class="label">E</div>
+    <div class="label">F</div>
+    <div class="label">G</div>
+    <div class="label">H</div>
+    <div class="label">I</div>
+    <div class="label">J</div>
+  </div>
+  <div class="row">
+    <div class="seat"></div>
+    <div class="seat"></div>
     <div class="seat"></div>
     <div class="seat"></div>
     <div class="seat"></div>
@@ -27,34 +42,6 @@ const seatControl = (props) => (
     <div class="seat"></div>
     <div class="seat"></div>
     <div class="seat"></div>
-  </div>
-  <div class="row">
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat occupied"></div>
-    <div class="seat occupied"></div>
-  </div>
-  <div class="row">
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-  </div>
-  <div class="row">
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat occupied"></div>
-    <div class="seat occupied"></div>
-    <div class="seat"></div>
     <div class="seat"></div>
     <div class="seat"></div>
   </div>
@@ -63,10 +50,49 @@ const seatControl = (props) => (
     <div class="seat"></div>
     <div class="seat"></div>
     <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat occupied"></div>
+    <div class="seat occupied"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+  </div>
+  <div class="row">
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+  </div>
+  <div class="row">
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat occupied"></div>
+    <div class="seat occupied"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+  </div>
+  <div class="row">
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
     <div class="seat occupied"></div>
     <div class="seat occupied"></div>
     <div class="seat occupied"></div>
     <div class="seat"></div>
+    <div class="seat"></div>
+    <div class="seat"></div>
+  </div>
   </div>
 </div>
 );
